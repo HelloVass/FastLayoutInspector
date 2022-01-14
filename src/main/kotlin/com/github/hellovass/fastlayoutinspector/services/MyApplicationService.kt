2dcff0cb781fb7e539d6313abfd3e2b5c28b30f6
GitHub Action <action@github.com>
2022-01-14 06:43:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hellovass/fastlayoutinspector/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hellovass.fastlayoutinspector.services
+
+import com.github.hellovass.fastlayoutinspector.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
